9e029ef96c5b6490dd2c9b888f3d4900286fec7f
Kevin Hernandez <kevinhdezvaz@gmail.com>
2026-01-05 12:51:28 -0600

    FIX - Video actualizado sin marca de agua

--- src/components/landing/PreferenceSelector.jsx
+++ src/components/landing/PreferenceSelector.jsx
@@ -84,12 +84,7 @@ const PreferenceSelector = () => {
                         </div>
 
                         {/* Indicador de video */}
-                        <div className="video-indicator">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                                <polygon points="5 3 19 12 5 21 5 3" />
-                            </svg>
-                            <span>Video en reproducción</span>
-                        </div>
+
                     </div>
 
                     {/* Columna derecha */}
